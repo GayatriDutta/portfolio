21a5916dfd608d50f470a3889d2862174cc5cc96
Gayatri Dutta <irtaya123@gmail.com>
2025-07-04 13:35:23 +0530

    reponsive skill page

--- src/app/skills/skills.ts
+++ src/app/skills/skills.ts
@@ -1,40 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import {MatTabsModule} from '@angular/material/tabs';
+import { Component, HostListener } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-skills',
   imports: [CommonModule, MatTabsModule],
   templateUrl: './skills.html',
-  styleUrl: './skills.css'
+  styleUrl: './skills.css',
 })
 export class Skills {
-    frameworks = [
-      'Angular', 
-      'React',
-      'Vue JS',      
-      'Nest JS',
-      'Express JS',
-    ];
-    programmingLanguge = [
-      'Javascript',
-      'Node JS', 
-      'Typescript',
-    ];
-    database = [
-      'MongoDB',
-      'Postgresql', 
-      'SQL',
-    ];
-    tools = [
-      'Git',
-      'AWS',
-      'Jira',
-    ];
-  selectedIndex : number = 0;
+  mobileView : boolean = true; 
+
+  @HostListener('window:resize', [])
+  onResize() {
+    this.mobileView = window.innerWidth< 537;
+  }
+  frameworks = ['Angular', 'React', 'Vue JS', 'Nest JS', 'Express JS'];
+  programmingLanguge = ['Javascript', 'Node JS', 'Typescript'];
+  database = ['MongoDB', 'Postgresql', 'SQL'];
+  tools = ['Git', 'AWS', 'Jira'];
+  selectedIndex: number = 0;
 
   onTabClick(index: number) {
-  this.selectedIndex = index;
-}
+    this.selectedIndex = index;
+  }
 
+  ngOnInit() {
+    this.mobileView = window.innerWidth <537;
+}
 }
